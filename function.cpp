f4a4dd174cab84d81543f47682b65caa4610bc40
Genz <69706255+Genz-47@users.noreply.github.com>
2021-06-15 15:46:55 +0700

    meunu

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Course.cpp"
+using namespace std;
+
+void menu(){
+    cout << "       MENU" << endl;
+    cout << "1.Danh sach hoc vien." << endl; 
+    cout << "2.Them 1 hoc vien moi." << endl; 
+    cout << "3.Xoa hoc vien." << endl; 
+    cout << "4.Sap xep theo DM1." << endl; 
+    cout << "5.Sap xep theo DM2." << endl; 
+    cout << "6.Sap xep theo DM1 và DM2." << endl; 
+    cout << "7.Danh sach theo ma mon hoc." << endl; 
+    cout << "8.Them mon hoc phu cho sv." << endl; 
+    cout << "9.Nhap diem mon hoc phu." << endl; 
+    cout << "10.Ghi vao file." << endl;
+    cout << "0.Thoat." << endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,33 +3,85 @@
 #include "SV.cpp"
 #include "Course.cpp"
 #include <iostream>
+#include "function.cpp"
 using namespace std;
 void docfile();
 int main(){
-    SV sv1("sv0001","Nguyen Van Dat");
-    sv1.setCourse("mh001","toan",9);
-    cout << sv1.toString();
-    // cout << sv1.getMaHV << "\n" << sv1.getHoTenHV << endl;
+    // SV sv1("sv0001","Nguyen Van Dat");
+    // sv1.setCourse("mh001","toan",9);
+    // cout << sv1.toString();
+    // // cout << sv1.getMaHV << "\n" << sv1.getHoTenHV << endl;
+    do
+    {
+        menu();
+        cout << "Nhap: ";
+        int i;
+        cin >> i;
+        switch (i)
+        {
+        case 0:
+            cout << "Ban that xu muon thoat:" << "\n Nhap 0 de thoat.\n Nhap 1 de qua lai menu";
+            cin >> i;
+            break;
+        case 1: 
+            cout << "       Danh sach hoc vien." << endl; 
+            break;
+        case 2: 
+            cout << "       Them 1 hoc vien moi." << endl; 
+            break;
+        case 3: 
+            cout << "       Xoa hoc vien." << endl; 
+            break;
+        case 4: 
+            cout << "       Sap xep theo DM1." << endl; 
+            break;
+        case 5: 
+            cout << "       Sap xep theo DM2." << endl; 
+            break;
+        case 6: 
+            cout << "       Sap xep theo DM1 và DM2." << endl; 
+            break;
+        case 7: 
+            cout << "       Danh sach theo ma mon hoc." << endl; 
+            break;
+        case 8: 
+            cout << "       Them mon hoc phu cho sv." << endl; 
+            break;
+        case 9: 
+            cout << "       Nhap diem mon hoc phụ." << endl; 
+            break;
+        case 10: 
+            cout << "       Ghi vao file." << endl; 
+            break;
+        default:
+            cout << "       Vui long nhap lai: ";
+            cin >> i;
+            break;
+        }
+        if(i == 0)
+            exit(0);
+    } while (true);
+    
     return 0;
 }
 void docfile(){
-    FILE *fi = fopen("hocvien.txt","r");
-    if(fi == NULL){
-        cout << "file khong ton tai.";
-        exit(0);
-    }
-    n = fgetc(fi) - '0';
-    char temp = fgetc(fi);
-    int i = 0, j = 0;
-    while(temp != EOF){
-        if(temp == '0' || temp == '1'){
-            if(j == n){
-                j = 0;
-                i++;
-            }  
-            A[i][j++] = temp -'0';
-        }
-        temp = fgetc(fi);
-    }
-    fclose(fi);
+    // FILE *fi = fopen("hocvien.txt","r");
+    // if(fi == NULL){
+    //     cout << "file khong ton tai.";
+    //     exit(0);
+    // }
+    // char n = fgetc(fi) - '0';
+    // char temp = fgetc(fi);
+    // int i = 0, j = 0;
+    // while(temp != EOF){
+    //     if(temp == '0' || temp == '1'){
+    //         if(j == n){
+    //             j = 0;
+    //             i++;
+    //         }  
+    //         A[i][j++] = temp -'0';
+    //     }
+    //     temp = fgetc(fi);
+    // }
+    // fclose(fi);
 }
